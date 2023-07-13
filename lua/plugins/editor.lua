d6b4bbbe17dcd16669be57fd984e0d17596fcd8f
Emman Nipal <emnnipal@gmail.com>
2023-07-14 00:21:24 +0800

    chore: add fzf telescope

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -39,6 +39,21 @@ return {
       wk.register(opts.defaults)
     end,
   },
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+    -- opts = {
+    --   defaults = {
+    --     theme = "dropdown",
+    --   },
+    -- },
+  },
 
   -- additional plugins
   {
