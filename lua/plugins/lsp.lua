bbf94335a66148c10c9b8404eeccde9c2dbbe47b
Emman Nipal <emnnipal@gmail.com>
2023-07-15 01:34:19 +0800

    chore: add border in hover doc and diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,12 +5,40 @@ return {
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- disable a keymap
       keys[#keys + 1] = { "K", false }
+
+      require("lazyvim.util").on_attach(function(_)
+        local border = "rounded"
+        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+          border = border,
+        })
+
+        vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+          border = border,
+        })
+
+        vim.diagnostic.config({
+          float = { border = border },
+        })
+      end)
     end,
     opts = {
-      -- autoformat = false,
+      -- autoformat = false, -- disable autoformat for lsp
       setup = {
         -- disable auto fix for eslint
         eslint = function() end,
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      lsp = {
+        hover = {
+          enabled = false, -- disable noice for overriding hover
+        },
+        signature = {
+          enabled = false, -- disable noice for overriding signature
+        },
       },
     },
   },
