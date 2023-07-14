e18f5661a0467858034406ec8e52cda9fef128b2
Emman Nipal <emnnipal@gmail.com>
2023-07-15 00:57:02 +0800

    chore: exclude javascript and typescript in tailwindcss

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       servers = {
         tailwindcss = {
-          filetypes_exclude = { "markdown" },
+          filetypes_exclude = { "markdown", "javascript", "typescript" },
         },
       },
       setup = {
