3ed08f1fdf9d3c0b4363fda12bad3422aa0a9f5a
Emman Nipal <emnnipal@gmail.com>
2023-07-14 10:50:15 +0800

    chore: replace tailwind extras

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -1,5 +1,35 @@
 return {
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {
+          filetypes_exclude = { "markdown" },
+        },
+      },
+      setup = {
+        tailwindcss = function(_, opts)
+          local tw = require("lspconfig.server_configurations.tailwindcss")
+          --- @param ft string
+          opts.filetypes = vim.tbl_filter(function(ft)
+            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+          end, tw.default_config.filetypes)
+
+          require("lazyvim.util").on_attach(function(client, bufnr)
+            if client.name == "tailwindcss" then
+              local tw_highlight = require("tailwind-highlight")
+              tw_highlight.setup(client, bufnr, {
+                single_column = false,
+                mode = "background",
+                debounce = 200,
+              })
+            end
+          end)
+        end,
+      },
+    },
+  },
+  {
     "princejoogie/tailwind-highlight.nvim",
     lazy = true,
   },
