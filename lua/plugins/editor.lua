2c8de95a478a6e425e42cc178c98e41bb76bfbef
Emman Nipal <emnnipal@gmail.com>
2023-07-15 11:03:11 +0800

    feat: make telescope in dropdown layout

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -48,17 +48,28 @@ return {
         require("telescope").load_extension("fzf")
       end,
     },
-    -- TODO: make telescope theme dropdown
-    -- opts = {
-    --   defaults = {
-    --     theme = "dropdown",
-    --   },
-    --   pickers = {
-    --     find_files = {
-    --       theme = "dropdown",
-    --     },
-    --   },
-    -- },
+    opts = {
+      defaults = {
+        results_title = false,
+        sorting_strategy = "ascending",
+        layout_strategy = "center",
+        layout_config = {
+          preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+          width = function(_, max_columns, _)
+            return math.min(max_columns, 84)
+          end,
+          height = function(_, _, max_lines)
+            return math.min(max_lines, 16)
+          end,
+        },
+        -- border = true,
+        -- borderchars = {
+        --   prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+        --   results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
+        --   preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        -- },
+      },
+    },
   },
   {
     "L3MON4D3/LuaSnip",
