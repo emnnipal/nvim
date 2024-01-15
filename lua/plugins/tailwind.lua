d2c6f93e130870410beafee2e2a3af2265666563
Emman Nipal <emnnipal@gmail.com>
2024-01-16 01:46:31 +0800

    chore: remove tailwind highlight as this might greatly contributes to slowness of neovim

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -7,30 +7,30 @@ return {
           filetypes_exclude = { "markdown", "javascript", "typescript" },
         },
       },
-      setup = {
-        tailwindcss = function(_, opts)
-          local tw = require("lspconfig.server_configurations.tailwindcss")
-          --- @param ft string
-          opts.filetypes = vim.tbl_filter(function(ft)
-            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
-          end, tw.default_config.filetypes)
-
-          require("lazyvim.util").lsp.on_attach(function(client, bufnr)
-            if client.name == "tailwindcss" then
-              local tw_highlight = require("tailwind-highlight")
-              tw_highlight.setup(client, bufnr, {
-                single_column = false,
-                mode = "background",
-                debounce = 200,
-              })
-            end
-          end)
-        end,
-      },
+      -- setup = {
+      --   tailwindcss = function(_, opts)
+      --     local tw = require("lspconfig.server_configurations.tailwindcss")
+      --     --- @param ft string
+      --     opts.filetypes = vim.tbl_filter(function(ft)
+      --       return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+      --     end, tw.default_config.filetypes)
+      --
+      --     require("lazyvim.util").lsp.on_attach(function(client, bufnr)
+      --       if client.name == "tailwindcss" then
+      --         local tw_highlight = require("tailwind-highlight")
+      --         tw_highlight.setup(client, bufnr, {
+      --           single_column = false,
+      --           mode = "background",
+      --           debounce = 200,
+      --         })
+      --       end
+      --     end)
+      --   end,
+      -- },
     },
   },
-  {
-    "princejoogie/tailwind-highlight.nvim",
-    lazy = true,
-  },
+  -- {
+  --   "princejoogie/tailwind-highlight.nvim",
+  --   lazy = true,
+  -- },
 }
