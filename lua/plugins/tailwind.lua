61bcb0b157a8220b725f4a6426ac36252f9f7c6b
Emman Nipal <emnnipal@gmail.com>
2024-01-28 18:46:30 +0800

    Revert "chore: remove tailwindcss as this makes editing in nvim very slow"
    This reverts commit 597160501a5292ee5736b09140791f8e5e6f297c.

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -1,34 +1,34 @@
 return {
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = {
-  --     servers = {
-  --       tailwindcss = {
-  --         filetypes = { "javascriptreact", "typescriptreact", "vue", "svelte", "html" },
-  --       },
-  --     },
-  --     -- setup = {
-  --     --   tailwindcss = function(_, opts)
-  --     --     local tw = require("lspconfig.server_configurations.tailwindcss")
-  --     --     --- @param ft string
-  --     --     opts.filetypes = vim.tbl_filter(function(ft)
-  --     --       return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
-  --     --     end, tw.default_config.filetypes)
-  --     --
-  --     --     require("lazyvim.util").lsp.on_attach(function(client, bufnr)
-  --     --       if client.name == "tailwindcss" then
-  --     --         local tw_highlight = require("tailwind-highlight")
-  --     --         tw_highlight.setup(client, bufnr, {
-  --     --           single_column = false,
-  --     --           mode = "background",
-  --     --           debounce = 200,
-  --     --         })
-  --     --       end
-  --     --     end)
-  --     --   end,
-  --     -- },
-  --   },
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {
+          filetypes = { "javascriptreact", "typescriptreact", "vue", "svelte", "html" },
+        },
+      },
+      -- setup = {
+      --   tailwindcss = function(_, opts)
+      --     local tw = require("lspconfig.server_configurations.tailwindcss")
+      --     --- @param ft string
+      --     opts.filetypes = vim.tbl_filter(function(ft)
+      --       return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+      --     end, tw.default_config.filetypes)
+      --
+      --     require("lazyvim.util").lsp.on_attach(function(client, bufnr)
+      --       if client.name == "tailwindcss" then
+      --         local tw_highlight = require("tailwind-highlight")
+      --         tw_highlight.setup(client, bufnr, {
+      --           single_column = false,
+      --           mode = "background",
+      --           debounce = 200,
+      --         })
+      --       end
+      --     end)
+      --   end,
+      -- },
+    },
+  },
   -- {
   --   "princejoogie/tailwind-highlight.nvim",
   --   lazy = true,
