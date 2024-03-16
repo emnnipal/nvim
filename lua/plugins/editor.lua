795632afd9c7e36fe74281c641333b95f55a7cb2
Emman Nipal <emnnipal@gmail.com>
2024-03-17 01:17:14 +0800

    chore: remove buffer words (text) in completion menu

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -121,7 +121,7 @@ return {
         { name = "nvim_lsp" },
         -- { name = "luasnip" }, -- For luasnip users.
       }, {
-        { name = "buffer" },
+        -- { name = "buffer" }, -- buffer words
       })
 
       opts.window = {
