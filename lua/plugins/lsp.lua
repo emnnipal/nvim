e8deffc4f2f695a6cd055e9e7157fba8ae2b7fee
Emman Nipal <emnnipal@gmail.com>
2024-05-13 13:30:59 +0800

    chore: update lazy lock

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -37,10 +37,13 @@ return {
     "williamboman/mason.nvim",
     opts = function(_, opts)
       -- Add your desired languages here to ensure they are installed
-      vim.list_extend(
-        opts.ensure_installed or {},
-        { "svelte-language-server", "prisma-language-server", "emmet-language-server", "css-lsp", "json-lsp" }
-      )
+      vim.list_extend(opts.ensure_installed or {}, {
+        "svelte-language-server",
+        "prisma-language-server",
+        "emmet-language-server",
+        "css-lsp",
+        "json-lsp",
+      })
     end,
   },
 }
