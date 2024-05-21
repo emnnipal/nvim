3d0e0b5e13c0bbfe6116d1d80d816ad5c5bdebf4
Emman Nipal <emnnipal@gmail.com>
2024-05-21 16:20:35 +0800

    chore: move treesitter list extends to lsp lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -47,4 +47,11 @@ return {
       })
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Add your desired syntax parsers here to ensure they are installed
+      vim.list_extend(opts.ensure_installed, { "svelte", "prisma" })
+    end,
+  },
 }
--- lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- Add your desired syntax parsers here to ensure they are installed
-      vim.list_extend(opts.ensure_installed, { "svelte", "prisma" })
-    end,
-  },
-}
