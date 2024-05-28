cd4ddf9f0eebe616405aa27de3cf0f2e6526bbd1
Emman Nipal <emnnipal@gmail.com>
2024-05-28 12:43:42 +0800

    chore: remove unused luasnip configuration

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -65,12 +65,6 @@ return {
     },
   },
   {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
-  {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-emoji",
