8f15f78d940e6166646891f37a34e97c7a9ad245
Emman Nipal <emnnipal@gmail.com>
2024-12-09 14:10:10 +0800

    chore: remove unnecessary overriding of colors in tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,8 @@
 return {
   {
     "folke/tokyonight.nvim",
-    lazy = true,
     opts = {
       on_highlights = function(highlights)
-        -- highlights.Comment = {
-        --   fg = "#5c6691",
-        --   style = "italic",
-        -- }
-        highlights.LineNr = {
-          fg = "#4e557b",
-        }
         highlights.CursorLineNr = {
           fg = "#9aa5ce",
         }
