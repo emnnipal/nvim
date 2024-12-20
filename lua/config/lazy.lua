92be4bbbeed7d4904158b59e437a5e5526c9f92b
Emman Nipal <emnnipal@gmail.com>
2024-12-20 19:46:30 +0800

    chore: move lazy plugin extras to lazyvim.json

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,16 +11,9 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.formatting.prettier" }, -- TODO: uncomment once prettierd is the one used as formatter instead of prettier
-    { import = "lazyvim.plugins.extras.util.project" },
-    { import = "lazyvim.plugins.extras.coding.blink" },
-    -- { import = "lazyvim.plugins.extras.coding.nvim-cmp" }, -- NOTE: enable via LazyExtras instead
-    { import = "lazyvim.plugins.extras.editor.fzf" },
-    -- { import = "lazyvim.plugins.extras.editor.telescope" },
+    -- { import = "lazyvim.plugins.extras.util.project" },
 
     -- import/override with your plugins
     { import = "plugins" },
