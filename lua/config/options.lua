884cf2937a2ca2e3fc0896f3a3953b6b1865835d
Emman Nipal <emnnipal@gmail.com>
2024-12-22 02:31:32 +0800

    chore: remove set termsync since the default value is true

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,7 +10,7 @@ vim.opt.conceallevel = 0
 
 -- Disable termsync to prevent buffering of screen updates during redraw cycles
 -- This is to address the lagging issue experienced in LazyGit within LazyVim
-vim.o.termsync = false
+-- vim.o.termsync = true
 
 vim.g.lazyvim_eslint_auto_format = false
 
