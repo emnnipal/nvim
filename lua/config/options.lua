b40728879b3eda98ac775bb7104f2ac4260cfb5c
Emman Nipal <emnnipal@gmail.com>
2025-01-22 09:37:13 +0800

    fix: disable termsync to leverage kitty/ghostty being fast

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,9 +8,7 @@ vim.o.pumblend = 0
 
 vim.opt.conceallevel = 0
 
--- Disable termsync to prevent buffering of screen updates during redraw cycles
--- This is to address the lagging issue experienced in LazyGit within LazyVim
--- vim.o.termsync = true
+vim.o.termsync = false
 
 vim.g.lazyvim_eslint_auto_format = false
 
