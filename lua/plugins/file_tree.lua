d4b1f4c32be76598897d276cb04b3b988545717a
Emman Nipal <emnnipal@gmail.com>
2025-02-10 20:02:43 +0800

    chore: make neo-tree keymaps use position float

--- lua/plugins/file_tree.lua
+++ lua/plugins/file_tree.lua
@@ -46,6 +46,20 @@ return {
         desc = "Explorer NeoTree (Root Dir)",
       },
       { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+      {
+        "<leader>ge",
+        function()
+          require("neo-tree.command").execute({ source = "git_status", toggle = true, position = "float" })
+        end,
+        desc = "Git Explorer",
+      },
+      {
+        "<leader>be",
+        function()
+          require("neo-tree.command").execute({ source = "buffers", toggle = true, position = "float" })
+        end,
+        desc = "Buffer Explorer",
+      },
     },
   },
 
