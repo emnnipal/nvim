59fd1c0d88c93f9d900ae4d507812f04a3829173
Emman Nipal <emnnipal@gmail.com>
2025-02-24 01:58:36 +0800

    fix: open neo-tree instead on initial load of neovim

--- lua/plugins/file-tree.lua
+++ lua/plugins/file-tree.lua
@@ -3,7 +3,30 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     enabled = LazyVim.has_extra("editor.neo-tree"),
-    init = function() end,
+    init = function()
+      -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
+      -- because `cwd` is not set up properly.
+      vim.api.nvim_create_autocmd("BufEnter", {
+        group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
+        desc = "Start Neo-tree with directory",
+        once = true,
+        callback = function()
+          if package.loaded["neo-tree"] then
+            return
+          else
+            local stats = vim.uv.fs_stat(vim.fn.argv(0))
+            if stats and stats.type == "directory" then
+              require("neo-tree.command").execute({
+                toggle = true,
+                dir = LazyVim.root(),
+                reveal = true,
+                position = "float",
+              })
+            end
+          end
+        end,
+      })
+    end,
     opts = {
       filesystem = {
         filtered_items = {
