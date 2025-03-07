9c96cf2bacb43c7afb4ec70e0622b8f366d93b99
Emman Nipal <emnnipal@gmail.com>
2025-03-07 14:28:45 +0800

    chore: hide bufferline buffer icons

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -7,9 +7,12 @@ return {
     "akinsho/bufferline.nvim",
     opts = {
       options = {
-        -- show_buffer_icons = false,
+        show_buffer_icons = false,
         show_buffer_close_icons = false,
         show_close_icon = false,
+        -- always_show_bufferline = false,
+        -- diagnostics = false,
+        -- diagnostics_update_on_event = false,
       },
     },
   },
