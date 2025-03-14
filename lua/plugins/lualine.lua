8548ab3a468c1aefab83b5aca3526f2afd938a08
Emman Nipal <emnnipal@gmail.com>
2025-03-14 14:20:36 +0800

    feat: use my custom statusline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,90 +1,88 @@
--- local M = {}
---
--- _G._statusline = M
---
--- local counter = 0
--- function M.diagnostic_status()
---   local ok = ""
---
---   local ignore = {
---     ["c"] = true, -- command mode
---     ["t"] = true, -- terminal mode
---   }
---
---   local mode = vim.api.nvim_get_mode().mode
---
---   if ignore[mode] then
---     return ok
---   end
---
---   local levels = vim.diagnostic.severity
---   local errors = #vim.diagnostic.get(0, { severity = levels.ERROR })
---   if errors > 0 then
---     return " ✘ "
---   end
---
---   local warnings = #vim.diagnostic.get(0, { severity = levels.WARN })
---   if warnings > 0 then
---     return " ▲ "
---   end
---
---   return ok
--- end
---
--- function M.active_lsp()
---   local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
---   if #buf_clients == 0 then
---     return "LSP Inactive"
---   end
---
---   local buf_client_names = {}
---
---   for _, client in pairs(buf_clients) do
---     table.insert(buf_client_names, client.name)
---   end
---
---   local unique_client_names = table.concat(buf_client_names, ", ")
---   local language_servers = string.format("[%s]", unique_client_names)
---
---   return language_servers
--- end
---
--- local modes = {
---   n = "NORMAL",
---   i = "INSERT",
---   v = "VISUAL",
---   V = "V-LINE",
---   c = "COMMAND",
---   R = "REPLACE",
---   s = "SELECT",
---   t = "TERMINAL",
--- }
---
--- function M.display_mode()
---   local current_mode = vim.api.nvim_get_mode().mode
---   return modes[current_mode] or current_mode
--- end
---
--- local statusline = {
---   " %{%v:lua._statusline.display_mode()%} ",
---   '%{expand("%:~:.")}', -- show current buffer file path relative to cwd
---   "%r",
---   "%m",
---   -- " - %{&filetype} ",
---   "%=",
---   "%{%v:lua._statusline.diagnostic_status()%}",
---   "%{%v:lua._statusline.active_lsp()%}",
---   -- " %2p%% ", -- show line position in percentage
---   -- " %3l:%-2c ", -- show cursor position in column and row number
---   -- '%{%v:lua._statusline_component("position")%}',
--- }
---
--- vim.o.statusline = table.concat(statusline, "")
+local M = {}
+
+_G._statusline = M
+
+function M.diagnostic_status()
+  local ok = ""
+
+  local ignore = {
+    ["c"] = true, -- command mode
+    ["t"] = true, -- terminal mode
+  }
+
+  local mode = vim.api.nvim_get_mode().mode
+
+  if ignore[mode] then
+    return ok
+  end
+
+  local levels = vim.diagnostic.severity
+  local errors = #vim.diagnostic.get(0, { severity = levels.ERROR })
+  if errors > 0 then
+    return " ✘ "
+  end
+
+  local warnings = #vim.diagnostic.get(0, { severity = levels.WARN })
+  if warnings > 0 then
+    return " ▲ "
+  end
+
+  return ok
+end
+
+function M.active_lsp()
+  local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
+  if #buf_clients == 0 then
+    return "LSP Inactive"
+  end
+
+  local buf_client_names = {}
+
+  for _, client in pairs(buf_clients) do
+    table.insert(buf_client_names, client.name)
+  end
+
+  local unique_client_names = table.concat(buf_client_names, ", ")
+  local language_servers = string.format("[%s]", unique_client_names)
+
+  return language_servers
+end
+
+local modes = {
+  n = "NORMAL",
+  i = "INSERT",
+  v = "VISUAL",
+  V = "V-LINE",
+  c = "COMMAND",
+  R = "REPLACE",
+  s = "SELECT",
+  t = "TERMINAL",
+}
+
+function M.display_mode()
+  local current_mode = vim.api.nvim_get_mode().mode
+  return modes[current_mode] or current_mode
+end
+
+local statusline = {
+  " %{%v:lua._statusline.display_mode()%} ",
+  '%{expand("%:~:.")}', -- show current buffer file path relative to cwd
+  "%r",
+  "%m",
+  -- " - %{&filetype} ",
+  "%=",
+  "%{%v:lua._statusline.diagnostic_status()%}",
+  "%{%v:lua._statusline.active_lsp()%}",
+  -- " %2p%% ", -- show line position in percentage
+  -- " %3l:%-2c ", -- show cursor position in column and row number
+}
+
+vim.o.statusline = table.concat(statusline, "")
 
 return {
   {
     "nvim-lualine/lualine.nvim",
-    -- enabled = false,
+    enabled = false,
     opts = function(_, opts)
       local icons = require("lazyvim.config").icons
 
