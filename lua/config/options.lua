fbb5ff300790f4299334bd6f2eca80b810b5c18a
Emman Nipal <emnnipal@gmail.com>
2025-03-14 14:24:27 +0800

    fix: hide unnecessay display of key presses and number of lines highlighted in command line

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -16,7 +16,8 @@ vim.g.snacks_animate = false
 
 vim.g.lazyvim_prettier_needs_config = true
 
+vim.o.showcmd = false -- Hide key presses showing in command line
+
 -- Fix for random alt key presses when pressing esc key if neovim is within tmux.
 vim.o.ttimeoutlen = 0
-
 -- vim.o.timeoutlen = 500 -- Adjust this if needed
