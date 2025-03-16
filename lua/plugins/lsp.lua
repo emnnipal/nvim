b93555736d2f4c22a127810a8656b307670e1a4e
Emman Nipal <emnnipal@gmail.com>
2025-03-16 22:43:25 +0800

    chore: refactor lsp lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -40,6 +40,27 @@ return {
     },
   },
   {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      -- Add your desired languages here to ensure they are installed
+      vim.list_extend(opts.ensure_installed or {}, {
+        "svelte-language-server",
+        "prisma-language-server",
+        "emmet-language-server",
+        "css-lsp",
+        "json-lsp",
+        "bash-language-server",
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Add your desired syntax parsers here to ensure they are installed
+      vim.list_extend(opts.ensure_installed, { "svelte", "prisma" })
+    end,
+  },
+  {
     "folke/noice.nvim",
     -- enabled = false,
     opts = {
@@ -85,26 +106,5 @@ return {
         },
       },
     },
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      -- Add your desired languages here to ensure they are installed
-      vim.list_extend(opts.ensure_installed or {}, {
-        "svelte-language-server",
-        "prisma-language-server",
-        "emmet-language-server",
-        "css-lsp",
-        "json-lsp",
-        "bash-language-server",
-      })
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      -- Add your desired syntax parsers here to ensure they are installed
-      vim.list_extend(opts.ensure_installed, { "svelte", "prisma" })
-    end,
   },
 }
