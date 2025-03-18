759cfd67991e00d0027fba46116bcf933a4b8400
Emman Nipal <emnnipal@gmail.com>
2025-03-18 23:19:38 +0800

    chore: remove cmp-nvim-lsp-signature-help

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -36,7 +36,7 @@ return {
       --  into multiple repos for maintenance purposes.
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
-      "hrsh7th/cmp-nvim-lsp-signature-help",
+      -- "hrsh7th/cmp-nvim-lsp-signature-help",
     },
     config = function()
       -- See `:help cmp`
