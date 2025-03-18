43e197ce37dd81033e058e4f324f7e0e36575d1b
Emman Nipal <emnnipal@gmail.com>
2025-03-18 23:37:04 +0800

    chore: enable snacks input

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -15,7 +15,7 @@ return {
       -- },
       explorer = { enabled = false },
       input = {
-        enabled = false,
+        enabled = true,
         icon_pos = false,
         win = {
           title_pos = "left",
