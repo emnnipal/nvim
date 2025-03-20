88febd2406a2c1eebe9dc743d090c9180ec38e29
Emman Nipal <emnnipal@gmail.com>
2025-03-20 16:43:20 +0800

    chore: adjust undo options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -93,6 +93,10 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 200 -- Save swap file and trigger CursorHold
+
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
