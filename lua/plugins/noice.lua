c089eb6fd94a1d63d8edd236e0587ad2faf7cbf3
Emman Nipal <emnnipal@gmail.com>
2025-03-28 14:25:32 +0800

    chore: disable signature in noice nvim since default signature help is now has a nice highlighting

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -19,15 +19,13 @@ return {
           -- Messages shown by lsp servers
           enabled = false,
         },
-        -- TODO: recreate hover border feature without using experimental noice.nvim
-        -- TODO: fix tailwindcss hover without noice.nvim. Without noice nvim, tailwind hover doesn't have treesitter.
+        -- TODO: fix tailwindcss hover without noice.nvim. Noice is able to use markdown for highlighting css markdown when hovering tailwind classes.
         hover = {
           -- enabled = false,
           silent = true, -- set to true to not show a message if hover is not available
         },
-        -- TODO: recreate signature help without noice.nvim
         signature = {
-          -- enabled = true,
+          enabled = false,
         },
       },
       presets = {
