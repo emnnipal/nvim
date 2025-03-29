0cec628ba56572acacdf5fbf61af790e092308bd
Emman Nipal <emnnipal@gmail.com>
2025-03-30 04:01:17 +0800

    chore: add delta in checkhealth

--- lua/core/health.lua
+++ lua/core/health.lua
@@ -21,7 +21,7 @@ end
 
 local check_external_reqs = function()
   -- Basic utils: `git`, `make`, `unzip`
-  for _, exe in ipairs({ "git", "make", "unzip", "rg", "lazygit", "fd" }) do
+  for _, exe in ipairs({ "git", "make", "unzip", "rg", "lazygit", "fd", "delta" }) do
     local is_executable = vim.fn.executable(exe) == 1
     if is_executable then
       vim.health.ok(string.format("Found executable: '%s'", exe))
