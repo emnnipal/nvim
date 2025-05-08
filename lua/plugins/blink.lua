2ff16d0ceedbbe4203a6a3385e0233c4b2c7a13b
Emman Nipal <emnnipal@gmail.com>
2025-05-09 01:48:35 +0800

    chore(blink): disable cmdline completion with blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -75,6 +75,7 @@ return {
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = false },
+      cmdline = { enabled = false },
     },
   },
 }
