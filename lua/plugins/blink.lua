462c6a48d36e2a79a4ffaf871efc922172dbebc9
Emman Nipal <emnnipal@gmail.com>
2025-05-09 01:39:28 +0800

    fix: set blink cmp label description highlights to comment color

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "BlinkCmpLabelDescription", { link = "Comment" })
+  end,
+})
+
 return {
   {
     "saghen/blink.cmp",
