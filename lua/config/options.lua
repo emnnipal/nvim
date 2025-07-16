1e99de5d972f531f6b880b2e2b35c83d62667670
Emman Nipal <emnnipal@gmail.com>
2025-07-16 23:34:40 +0800

    chore: enable smooth scroll

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -103,3 +103,5 @@ vim.o.updatetime = 200 -- Save swap file and trigger CursorHold
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+vim.o.smoothscroll = true
