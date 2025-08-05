bbd23a45d7e107440e35b6d3f3620e2c0e801807
Emman Nipal <emnnipal@gmail.com>
2025-08-06 00:35:19 +0800

    chore: add my old and initial config for telescope when overriding lazyvim telescope extra

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,1 +1,25 @@
-return {}
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    enabled = vim.g.picker_plugin == "telescope",
+    opts = {
+      defaults = {
+        results_title = false,
+        sorting_strategy = "ascending",
+        layout_strategy = "center",
+        layout_config = {
+          preview_cutoff = 1, -- Preview should always show (unless previewer = false)
+          width = function(_, max_columns, _)
+            return math.min(max_columns, 110)
+          end,
+          height = function(_, _, max_lines)
+            return math.min(max_lines, 20)
+          end,
+        },
+      },
+    },
+    keys = {
+      { "<leader>s/", "<Cmd>Telescope current_buffer_fuzzy_find<CR>", desc = "Search in current buffer" },
+    },
+  },
+}
