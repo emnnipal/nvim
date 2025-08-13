5de6ca99d1b581c5bf751d4abb7517e8f41855ba
Emman Nipal <emnnipal@gmail.com>
2025-08-14 05:14:22 +0800

    chore: enhance tab keymap in nvim-ix

--- lua/plugins/nvim-ix.lua
+++ lua/plugins/nvim-ix.lua
@@ -61,25 +61,33 @@ return {
       },
     })
 
+    local function map_completion_key(key, action)
+      vim.keymap.set({ "i", "c" }, key, function()
+        ix.do_action(function(ctx)
+          local selection = ctx.completion.get_selection()
+          if ctx.completion.is_menu_visible() and selection then
+            action(ctx, selection)
+          else
+            -- fall back to the actual key if the menu is not visible
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, false, true), "n", true)
+          end
+        end)
+      end)
+    end
+
+    map_completion_key("<Tab>", function(ctx, selection)
+      ctx.completion.select(selection.index + 1)
+    end)
+
+    map_completion_key("<S-Tab>", function(ctx, selection)
+      ctx.completion.select(selection.index - 1)
+    end)
+
     vim.keymap.set({ "i", "c" }, "<C-f>", ix.action.scroll(0 + 3))
     vim.keymap.set({ "i", "c" }, "<C-b>", ix.action.scroll(0 - 3))
 
     vim.keymap.set({ "i", "c" }, "<C-Space>", ix.action.completion.complete())
 
-    -- vim.keymap.set({ "i", "c" }, "<Tab>", ix.action.completion.select_next())
-    vim.keymap.set({ "i", "c" }, "<Tab>", function()
-      ix.do_action(function(ctx)
-        local selection = ctx.completion.get_selection()
-        if ctx.completion.is_menu_visible() and selection then
-          ctx.completion.select(selection.index + 1)
-        else
-          -- fall back to Tab if the menu is not visible
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", true)
-        end
-      end)
-    end)
-
-    vim.keymap.set({ "i", "c" }, "<S-Tab>", ix.action.completion.select_prev())
     vim.keymap.set({ "i", "c" }, "<C-e>", ix.action.completion.close())
     ix.charmap.set("c", "<CR>", ix.action.completion.commit_cmdline())
     ix.charmap.set("i", "<CR>", ix.action.completion.commit())
