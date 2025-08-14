56d080cf25e1a736b4d50cdc4488519ef4e4cc1b
Emman Nipal <emnnipal@gmail.com>
2025-08-14 22:40:43 +0800

    chore: add note on how to get server capabilities

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -37,6 +37,28 @@ end
 --- --- Checks if the attached LSP client(s) have the specified capability.
 --- --- @param capabilities string|string[]: The capability or list of capabilities to check.
 --- --- @return boolean
+---
+--- TODO:
+--- To see the capabilities for a given server, try this in a LSP-enabled buffer:
+--- :lua =vim.lsp.get_clients()[1].server_capabilities
+--  Ref: https://neovim.io/doc/user/lsp.html#lsp-attach
+---
+--- TODO:
+--- To check the capabilities for a given server
+--  if
+--    not client:supports_method("textDocument/willSaveWaitUntil")
+--    and client:supports_method("textDocument/formatting")
+--  then
+--    vim.api.nvim_create_autocmd("BufWritePre", {
+--      group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
+--      buffer = args.buf,
+--      callback = function()
+--        vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+--      end,
+--    })
+--  end
+--  Ref: https://neovim.io/doc/user/lsp.html#lsp-attach
+---
 --- function M.check_capabilities(capabilities)
 ---   local clients = vim.lsp.get_active_clients({ bufnr = 0 })
 ---   if not clients or vim.tbl_isempty(clients) then
