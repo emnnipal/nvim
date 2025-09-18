c9a62d6d66cccf94b0ef83430faac30795e3b8a4
Emman Nipal <emnnipal@gmail.com>
2025-09-18 18:34:50 +0800

    chore: make sure tree-sitter is installed

--- lua/core/health.lua
+++ lua/core/health.lua
@@ -14,7 +14,7 @@ end
 
 local check_external_reqs = function()
   -- Basic utils: `git`, `make`, `unzip`
-  for _, exe in ipairs({ "git", "make", "unzip", "rg", "lazygit", "fd", "delta" }) do
+  for _, exe in ipairs({ "git", "make", "unzip", "rg", "lazygit", "fd", "delta", "tree-sitter" }) do
     local is_executable = vim.fn.executable(exe) == 1
     if is_executable then
       vim.health.ok(string.format("Found executable: '%s'", exe))
