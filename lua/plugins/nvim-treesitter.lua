f81bcc449d8a22f8d719ef4991b651e293418221
Emman Nipal <emnnipal@gmail.com>
2025-09-18 14:41:57 +0800

    feat: replace nvim-treesitter incremental selection with wildrift.nvim since it's no longer supported in treesitter main branch

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -33,15 +33,6 @@ return {
         "xml",
         "yaml",
       },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
 
       -- Autoinstall languages that are not installed
       auto_install = true,
@@ -53,29 +44,24 @@ return {
         additional_vim_regex_highlighting = { "ruby" },
       },
       indent = { enable = true, disable = { "ruby" } },
-      -- textobjects = {
-      --   move = {
-      --     enable = true,
-      --     goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-      --     goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-      --     goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-      --     goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-      --   },
-      -- },
-    },
-    keys = {
-      { "<c-space>", desc = "Increment Selection" },
-      { "<bs>", desc = "Decrement Selection", mode = "x" },
     },
   },
 
   {
-    "folke/which-key.nvim",
+    -- TODO: Replace with another incremental selection plugin since this one hasn’t been updated for 2 years
+    "sustech-data/wildfire.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
     opts = {
-      spec = {
-        { "<BS>", desc = "Decrement Selection", mode = "x" },
-        { "<c-space>", desc = "Increment Selection", mode = { "x", "n" } },
+      keymaps = {
+        init_selection = "<C-Space>",
+        node_incremental = "<C-Space>",
+        node_decremental = "<BS>",
       },
+    },
+    keys = {
+      { "<BS>", desc = "Decrement Selection", mode = "x" },
+      { "<c-space>", desc = "Increment Selection", mode = { "x", "n", "o" } },
     },
   },
 }
