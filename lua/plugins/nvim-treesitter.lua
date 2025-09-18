96667fc37e3da2debf6db5013d618661014469a2
Emman Nipal <emnnipal@gmail.com>
2025-09-18 19:06:43 +0800

    feat: replace nvim-treesitter incremental selection in preparation of main branch nvim-treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -33,15 +33,6 @@ return {
         "xml",
         "yaml",
       },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
 
       -- Autoinstall languages that are not installed
       auto_install = true,
@@ -53,19 +44,14 @@ return {
         additional_vim_regex_highlighting = { "ruby" },
       },
       indent = { enable = true, disable = { "ruby" } },
-      -- textobjects = {
-      --   move = {
-      --     enable = true,
-      --     goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-      --     goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-      --     goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-      --     goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-      --   },
-      -- },
     },
-    keys = {
-      { "<c-space>", desc = "Increment Selection" },
-      { "<bs>", desc = "Decrement Selection", mode = "x" },
+  },
+
+  {
+    "daliusd/incr.nvim",
+    opts = {
+      incr_key = "<C-Space>", -- increment selection key
+      decr_key = "<BS>", -- decrement selection key
     },
   },
 
